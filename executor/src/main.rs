489e70ec4060975a4df8490dfc9583fd05f51d06
Martin E. Zulliger <exe@patchnotes.com>
2022-08-01 22:18:01 +0000

    lint

--- executor/src/main.rs
+++ executor/src/main.rs
@@ -1,10 +1,9 @@
-use mu::gossip::{Gossip, Node};
+use mu::gossip::Node;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
 
-    let n = Node::new("0.0.0.0", 59999);
-    //    let mut g = Gossip::new(n, Default::default());
-    //    g.start().await.unwrap();
+    let _n = Node::new("0.0.0.0", 59999);
+    //    let mut g = Gossip::new(n, Default::default()).await.unwrap();
 }
