9ab87953ff1e1279a34afce769e9e2128f5742e2
Arshia Ghafoori <arshia001@live.com>
2022-08-04 15:47:00 +0430

    Move tokio_mailbox_processor into main repo

---
+++ tokio_mailbox_processor/src/lib.rs
@@ -0,0 +1,41 @@
+use thiserror::Error;
+use tokio::sync::oneshot;
+
+pub mod callback;
+pub mod plain;
+
+#[derive(Debug, PartialEq, Error)]
+pub enum Error {
+    #[error("Mailbox is stopped")]
+    MailboxStopped,
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+// This function is used to ignore errors when sending something
+// from inside the mailbox. The receiving end may have panicked,
+// but we don't want to fail the entire mailbox if they do.
+fn ignore_error<E>(_r: std::result::Result<(), E>) {}
+
+/// Used to return data from a message.
+///
+/// `ReplyChannel`s are constructed by [`MailboxProcessor::post_and_reply`].
+/// `ReplyChannel`s mut be embedded in the message itself.
+/// The step function can take the `ReplyChannel` out of the message and use the
+/// [`ReplyChannel::reply`] function to send back a reply.
+///
+/// The reply channel is also used to detect when the
+/// message has been received and processed by the mailbox, so it's best to provide
+/// the reply late in the message's processing logic. The reply channel should always
+/// be used; otherwise, `post_and_reply` will report a `MailboxStopped` error when it's
+/// dropped. See the documentation for [`MailboxProcessor`] for a code sample featuring
+/// `ReplyChannel`s.
+pub struct ReplyChannel<T> {
+    sender: oneshot::Sender<T>,
+}
+
+impl<T> ReplyChannel<T> {
+    pub fn reply(self, val: T) {
+        ignore_error(self.sender.send(val));
+    }
+}
