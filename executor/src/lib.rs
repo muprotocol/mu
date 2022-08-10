caa8601cbbbd5a98b28a500e5a14c5a79c9e4e4f
Arshia Ghafoori <arshia001@live.com>
2022-08-10 12:11:23 +0430

    Add Ctrl+C handling, refactor

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -1,20 +1,26 @@
-mod config;
-mod connection_manager;
 pub mod gossip;
-mod log_setup;
+mod infrastructure;
+mod network;
 pub mod runtime;
+
+use std::process;
 
 use anyhow::{Context, Result};
 use async_trait::async_trait;
 use bytes::Bytes;
-
 use log::*;
+use tokio_util::sync::CancellationToken;
 
-use connection_manager::ConnectionID;
-
-use crate::connection_manager::ConnectionManager;
+use infrastructure::{config, log_setup};
+use network::connection_manager::{self, ConnectionID, ConnectionManager, ConnectionManagerConfig};
 
 pub async fn run() -> Result<()> {
+    let cancellation_token = CancellationToken::new();
+    let cancellation_token_clone = cancellation_token.clone();
+
+    ctrlc::set_handler(move || cancellation_token_clone.cancel())
+        .context("Failed to initialize Ctrl+C handler")?;
+
     let config = config::initialize_config(vec![
         ("log_level", "warn"),
         ("connection_manager.listen_ip", "0.0.0.0"),
@@ -25,7 +31,7 @@ pub async fn run() -> Result<()> {
 
     info!("Initializing Mu...");
 
-    let connection_manager_config = connection_manager::ConnectionManagerConfig {
+    let connection_manager_config = ConnectionManagerConfig {
         listen_address: config
             .get_string("connection_manager.listen_address")?
             .parse()
@@ -39,57 +45,33 @@ pub async fn run() -> Result<()> {
 
     let mut connection_manager = connection_manager::new();
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
+    connection_manager.set_callbacks(CB());
 
-    connection_manager.set_callbacks(CB(
-        tx.clone(),
-        connection_manager_config.listen_port == 12012,
-    ));
+    if cancellation_token.is_cancelled() {
+        process::exit(0);
+    }
 
     connection_manager
         .start(connection_manager_config)
         .await
         .context("Failed to start connection manager")?;
 
-    // do something!
-    let port = config
-        .get_string("connection_manager.listen_port")?
-        .parse::<i32>()?;
-    if port == 12012 {
-        loop {
-            if let Ok(id) = rx.try_recv() {
-                dbg!("################################ ML1");
-                tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
-                let b = connection_manager
-                    .send_req_rep(id, Bytes::copy_from_slice(b"OOOOOOH!"))
-                    .await?;
-                println!("Received looped req-rep {}", String::from_utf8_lossy(&b));
-                dbg!("################################ ML2");
-            }
-        }
-    } else {
-        let id = loop {
-            match connection_manager
-                .connect("127.0.0.1".parse()?, 12012)
-                .await
-            {
-                Ok(x) => break x,
-                Err(f) => error!("Failed to connect due to {}, will retry", f),
-            }
-        };
-
-        let data = "Hello!".into();
-        connection_manager.send_datagram(id, data).await?;
-
-        let data = "Hello!".into();
-        let rep = connection_manager.send_req_rep(id, data).await?;
-        info!("Received reply: {}", String::from_utf8_lossy(&rep));
-
-        info!("Sleeping for 6 seconds in case the server has something to say");
-        tokio::time::sleep(std::time::Duration::from_secs(6)).await;
-
-        connection_manager.stop().await?;
+    if cancellation_token.is_cancelled() {
+        process::exit(0);
     }
+
+    // Connect to and query seed nodes
+    // Start gossip
+
+    // TODO handle failures in components
+
+    cancellation_token.cancelled().await;
+    info!("Received SIGINT, stopping");
+
+    connection_manager
+        .stop()
+        .await
+        .context("Failed to stop connection manager")?;
 
     info!("Goodbye!");
 
@@ -97,7 +79,7 @@ pub async fn run() -> Result<()> {
 }
 
 #[derive(Clone)]
-struct CB(tokio::sync::mpsc::Sender<ConnectionID>, bool);
+struct CB();
 
 #[async_trait]
 impl connection_manager::ConnectionManagerCallbacks for CB {
@@ -110,7 +92,7 @@ impl connection_manager::ConnectionManagerCallbacks for CB {
     }
 
     async fn datagram_received(&self, id: ConnectionID, data: Bytes) {
-        info!(
+        debug!(
             "Datagram received from {}: {}",
             id,
             String::from_utf8_lossy(&data)
@@ -118,20 +100,12 @@ impl connection_manager::ConnectionManagerCallbacks for CB {
     }
 
     async fn req_rep_received(&self, id: ConnectionID, data: Bytes) -> Bytes {
-        dbg!("################################ RR1");
-        info!(
-            "Req/Rep received from {}: {}",
+        debug!(
+            "Req-rep received from {}: {}",
             id,
             String::from_utf8_lossy(&data)
         );
 
-        if self.1 {
-            self.0.send(id).await.unwrap();
-        }
-
-        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
-
-        dbg!("################################ RR2");
         data
     }
 }
