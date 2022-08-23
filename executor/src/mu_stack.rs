f96b07acf2751f7f91bc6a5352009799fcc5ab0c
Arshia Ghafoori <arshia001@live.com>
2022-08-23 15:23:56 +0430

    WIP: stack deployment

--- executor/src/mu_stack.rs
+++ executor/src/mu_stack.rs
@@ -1,13 +1,48 @@
+pub mod deploy;
+
 // We must use a BTreeMap to ensure key ordering stays consistent.
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, fmt::Display};
 
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub struct StackID(pub Uuid);
+
+impl Display for StackID {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Stack {
     pub name: String,
     pub version: String,
     pub services: Vec<Service>,
+}
+
+impl Stack {
+    pub fn databases(&self) -> impl Iterator<Item = &Database> {
+        self.services.iter().filter_map(|s| match s {
+            Service::Database(db) => Some(db),
+            _ => None,
+        })
+    }
+
+    pub fn functions(&self) -> impl Iterator<Item = &Function> {
+        self.services.iter().filter_map(|s| match s {
+            Service::Function(func) => Some(func),
+            _ => None,
+        })
+    }
+
+    pub fn gateways(&self) -> impl Iterator<Item = &Gateway> {
+        self.services.iter().filter_map(|s| match s {
+            Service::Gateway(gw) => Some(gw),
+            _ => None,
+        })
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
