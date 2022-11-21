41b688ef41a9e3b1aa086966a83a3a37e3831567
Arshia Ghafoori <arshia001@live.com>
2022-11-21 11:05:32 +0330

    Yet another database deployment fix

--- executor/src/stack/deploy.rs
+++ executor/src/stack/deploy.rs
@@ -156,7 +156,7 @@ pub(super) async fn deploy(
         .await
         .unwrap_or(());
 
-    let prefix = format!("{id}_");
+    let prefix = format!("{id}!");
     for db_id in db_service.query_db_by_prefix(&prefix).unwrap_or_default() {
         if !db_ids.contains(&db_id) {
             db_service.drop_db(&db_id).await.unwrap_or(());
