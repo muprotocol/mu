e93754f02f515d66e7a07d78dfc17defb05b3f95
Arshia Ghafoori <arshia001@live.com>
2023-01-08 15:09:30 +0330

    Add gossip proto

--- executor/build.rs
+++ executor/build.rs
@@ -4,6 +4,7 @@ fn main() {
         .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
         .includes(["protos", "../mu_stack/protos"])
         .input("protos/rpc.proto")
+        .input("protos/gossip.proto")
         .cargo_out_dir("protos")
         .run_from_script();
 }
