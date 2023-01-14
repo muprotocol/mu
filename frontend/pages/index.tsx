4eafb8d832e97bc7add40430874119f484cc7e7b
naixatwork <naixatwork@gmail.com>
2023-01-14 19:24:18 +0330

    feat(jest): installed jest testing library

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import React from "react";
 
 export default function Home() {
     return (
@@ -9,7 +10,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main>
+            <main data-testid="content">
                 <h1 className="text-3xl font-bold underline">
                     Hello world!
                 </h1>
