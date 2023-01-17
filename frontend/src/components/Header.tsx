32c087136f26a53ccf3bfe9df0f64503ada0ce10
naixatwork <naixatwork@gmail.com>
2023-01-17 22:25:29 +0330

    feat(global): added .env files

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,13 @@
+import * as process from "process";
+
 export default function Header() {
+    console.log(process.env.NEXT_PUBLIC_API)
     return (
-        <div className="flex justify-between">lol</div>
+        <header className="container mx-auto p-5 flex justify-between">
+            <div>logo</div>
+            <div>
+                button
+            </div>
+        </header>
     )
 }
