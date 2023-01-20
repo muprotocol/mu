1605fe13128c371988836669bb0c1291087fb367
naixatwork <naixatwork@gmail.com>
2023-01-20 18:01:49 +0330

    feat(vitest): added path alias in vitest.config.ts

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: 'jsdom',
+        globals: true,
+    },
+    resolve: {
+        alias: {
+            '@': "./src",
+        }
     },
 })
