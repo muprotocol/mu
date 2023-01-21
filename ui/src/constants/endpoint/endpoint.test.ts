e21423a2741644db7552f3dc27c01f062b99e4a5
naixatwork <naixatwork@gmail.com>
2023-01-21 13:58:06 +0330

    fix(endpoint): fixed an issue where accessing process.env with spreading object was returning null

--- ui/src/constants/endpoint/endpoint.test.ts
+++ ui/src/constants/endpoint/endpoint.test.ts
@@ -1,7 +1,7 @@
 import {describe, test, expect} from "vitest";
 import endpoint from "./endpoint"
 
-describe("endpoint", () => {
+describe("!endpoint", () => {
     test("it should return #NEXT_PUBLIC_API", () => {
         expect(endpoint()).toBe(process.env.VITE_NEXT_PUBLIC_API);
     })
