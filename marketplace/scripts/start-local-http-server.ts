9dfbd362297dd89019412a81806004de084d4ac4
Arshia Ghafoori <arshia001@live.com>
2023-01-23 18:21:41 +0330

    Add test stack v2 + update/delete scripts

--- marketplace/scripts/start-local-http-server.ts
+++ marketplace/scripts/start-local-http-server.ts
@@ -5,4 +5,7 @@ run("rustup target add wasm32-wasi");
 run(`cd ${path.resolve(__dirname, "test-stack", "test-function")} && ` +
     `cargo build --release --target wasm32-wasi && ` +
     `cp ./target/wasm32-wasi/release/test-function.wasm ..`);
+run(`cd ${path.resolve(__dirname, "test-stack", "test-function-v2")} && ` +
+    `cargo build --release --target wasm32-wasi && ` +
+    `cp ./target/wasm32-wasi/release/test-function-v2.wasm ..`);
 run(`cd ${path.resolve(__dirname, "test-stack")} && npx http-server`);
