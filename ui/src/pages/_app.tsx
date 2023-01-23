5cd7bb87ec4dcd370ca91024a62851e40e74e3c0
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-01-23 18:04:27 +0330

    feat(prettier): config prettier

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,19 +1,15 @@
 import type { AppProps } from "next/app";
+
 import Header from "@/components/layout/Header";
 import WalletWrapper from "@/components/wallet/WalletWrapper/WalletWrapper";
+
 import "@/styles/globals.css";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { lightTheme } from "../themes/lightTheme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-
-      <WalletWrapper>
-        <Header />
-        <Component {...pageProps} />
-      </WalletWrapper>
-    </ThemeProvider>
+    <WalletWrapper>
+      <Header />
+      <Component {...pageProps} />
+    </WalletWrapper>
   );
 }
