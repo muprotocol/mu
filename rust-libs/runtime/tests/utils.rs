99459aa3e850123ea0a9d9a86189dd69d228fcda
Hamid R. K. Pisghadam <hamidrkp@riseup.net>
2023-01-29 02:38:18 +0330

    WIP!

--- rust-libs/runtime/tests/utils.rs
+++ rust-libs/runtime/tests/utils.rs
@@ -1,18 +1,19 @@
 use std::{
+    borrow::Cow,
     collections::{hash_map::Entry, HashMap},
-    env, fs,
-    net::IpAddr,
+    env::temp_dir,
+    net::{IpAddr, Ipv4Addr},
     path::{Path, PathBuf},
     process::Command,
-    str::FromStr,
-    sync::Arc,
+    sync::{Arc, Mutex},
 };
 
-use anyhow::{bail, Context, Result};
+use anyhow::Result;
 use mu_db::{
     DbManager, DbManagerImpl, IpAndPort, NodeAddress, PdConfig, TikvConfig, TikvRunnerConfig,
 };
-use tokio::sync::Mutex;
+use musdk_common::Header;
+use rstest::fixture;
 
 use async_trait::async_trait;
 use mu_runtime::{
@@ -20,23 +21,34 @@ use mu_runtime::{
 };
 use mu_stack::{AssemblyID, AssemblyRuntime, FunctionID, StackID};
 
-#[derive(Default)]
+// Add test project names (directory name) in this array to build them when testing
+const TEST_PROJECTS: &'static [&'static str] = &[
+    "hello-wasm",
+    "calc-func",
+    "multi-body",
+    "unclean-termination",
+];
+
+#[derive(Default, Clone)]
 pub struct MapAssemblyProvider {
-    inner: HashMap<AssemblyID, AssemblyDefinition>,
+    inner: Arc<Mutex<HashMap<AssemblyID, AssemblyDefinition>>>,
 }
 
 #[async_trait]
 impl AssemblyProvider for MapAssemblyProvider {
-    fn get(&self, id: &AssemblyID) -> Option<&AssemblyDefinition> {
-        Some(self.inner.get(id).unwrap())
+    fn get(&self, _id: &AssemblyID) -> Option<&AssemblyDefinition> {
+        unimplemented!("Not needed")
     }
 
     fn add_function(&mut self, function: AssemblyDefinition) {
-        self.inner.insert(function.id.clone(), function);
+        self.inner
+            .lock()
+            .unwrap()
+            .insert(function.id.clone(), function);
     }
 
-    fn remove_function(&mut self, id: &AssemblyID) {
-        self.inner.remove(id);
+    fn remove_function(&mut self, _id: &AssemblyID) {
+        unimplemented!("Not needed")
     }
 
     fn get_function_names(&self, _stack_id: &StackID) -> Vec<String> {
@@ -44,60 +56,11 @@ impl AssemblyProvider for MapAssemblyProvider {
     }
 }
 
-const TEST_DATA_DIR: &str = "tests/runtime/test_data";
+fn get_temp_dir() -> PathBuf {
+    let rand: [u8; 32] = rand::random();
+    let rand = String::from_utf8_lossy(&rand);
 
-fn clean_data_dir() {
-    match fs::remove_dir_all(TEST_DATA_DIR) {
-        Ok(()) => (),
-        Err(e) if e.kind() == std::io::ErrorKind::NotFound => (),
-        Err(e) => Err(e).unwrap(),
-    }
-}
-
-fn ensure_project_dir(project_dir: &Path) -> Result<PathBuf> {
-    let project_dir = env::current_dir()?.join(project_dir);
-    if !project_dir.is_dir() {
-        bail!("project dir should be a directory")
-    }
-    Ok(project_dir)
-}
-
-async fn install_wasm32_wasi_target() -> Result<()> {
-    Command::new("rustup")
-        .args(["target", "add", "wasm32-wasi"])
-        .spawn()?
-        .wait()?;
-    Ok(())
-}
-
-pub async fn compile_wasm_project(project_dir: &Path) -> Result<()> {
-    let project_dir = ensure_project_dir(project_dir)?;
-    install_wasm32_wasi_target().await?;
-
-    Command::new("cargo")
-        .current_dir(&project_dir)
-        .env_remove("CARGO_TARGET_DIR")
-        .arg("build")
-        .args(["--release", "--target", "wasm32-wasi"])
-        .spawn()?
-        .wait()?;
-
-    Ok(())
-}
-
-//TODO: maybe some `make clean` usage for this function
-#[allow(dead_code)]
-pub async fn clean_wasm_project(project_dir: &Path) -> Result<()> {
-    let project_dir = ensure_project_dir(project_dir)?;
-
-    Command::new("cargo")
-        .current_dir(&project_dir)
-        .env_remove("CARGO_TARGET_DIR")
-        .arg("clean")
-        .spawn()?
-        .wait()?;
-
-    Ok(())
+    temp_dir().join(format!("/{rand}"))
 }
 
 pub struct Project<'a> {
@@ -127,16 +90,6 @@ impl<'a> Project<'a> {
     }
 }
 
-pub async fn build_wasm_projects<'a>(projects: &'a [Project<'a>]) -> Result<()> {
-    for p in projects {
-        compile_wasm_project(&p.path)
-            .await
-            .context("compile wasm project")?
-    }
-
-    Ok(())
-}
-
 pub async fn read_wasm_functions<'a>(
     projects: &'a [Project<'a>],
 ) -> Result<HashMap<AssemblyID, AssemblyDefinition>> {
@@ -160,96 +113,231 @@ pub async fn read_wasm_functions<'a>(
     Ok(results)
 }
 
-async fn create_map_function_provider<'a>(
-    projects: &'a [Project<'a>],
-) -> Result<(HashMap<AssemblyID, AssemblyDefinition>, MapAssemblyProvider)> {
-    build_wasm_projects(projects).await?;
-    let functions = read_wasm_functions(projects).await?;
-    Ok((functions, MapAssemblyProvider::default()))
-}
+pub mod fixture {
+    use super::*;
 
-fn make_node_address(port: u16) -> NodeAddress {
-    NodeAddress {
-        address: "127.0.0.1".parse().unwrap(),
-        port,
+    macro_rules! block_on {
+        ($async_expr:expr) => {{
+            tokio::task::block_in_place(|| {
+                let handle = tokio::runtime::Handle::current();
+                handle.block_on($async_expr)
+            })
+        }};
     }
-}
-fn make_tikv_runner_conf(peer_port: u16, client_port: u16, tikv_port: u16) -> TikvRunnerConfig {
-    let localhost: IpAddr = "127.0.0.1".parse().unwrap();
-    TikvRunnerConfig {
-        pd: PdConfig {
-            peer_url: IpAndPort {
-                address: localhost,
-                port: peer_port,
-            },
-            client_url: IpAndPort {
-                address: localhost,
-                port: client_port,
-            },
-            data_dir: format!("{TEST_DATA_DIR}/pd_data_dir_{peer_port}"),
-            log_file: Some(format!("{TEST_DATA_DIR}/pd_log_{peer_port}")),
-        },
-        node: TikvConfig {
-            cluster_url: IpAndPort {
-                address: localhost,
-                port: tikv_port,
-            },
-            data_dir: format!("{TEST_DATA_DIR}/tikv_data_dir_{tikv_port}"),
-            log_file: Some(format!("{TEST_DATA_DIR}/tikv_log_{tikv_port}")),
-        },
+
+    #[fixture]
+    #[once]
+    pub fn install_wasm32_wasi_target_fixture() {
+        Command::new("rustup")
+            .args(["target", "add", "wasm32-wasi"])
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
     }
-}
 
-pub async fn create_runtime<'a>(
-    projects: &'a [Project<'a>],
-) -> (
-    Box<dyn Runtime>,
-    Box<dyn DbManager>,
-    Arc<Mutex<HashMap<StackID, Usage>>>,
-) {
-    clean_data_dir();
-    let config = RuntimeConfig {
-        cache_path: PathBuf::from_str("runtime-cache").unwrap(),
-        include_function_logs: true,
-    };
+    #[fixture]
+    #[once]
+    pub fn build_test_funcs_fixture() {
+        for name in TEST_PROJECTS {
+            let project_dir = format!("tests/funcs/{name}");
+            Command::new("cargo")
+                .current_dir(project_dir)
+                .env_remove("CARGO_TARGET_DIR")
+                .arg("build")
+                .args(["--release", "--target", "wasm32-wasi"])
+                .spawn()
+                .unwrap()
+                .wait()
+                .unwrap();
+        }
+    }
 
-    let (functions, provider) = create_map_function_provider(projects).await.unwrap();
-    let node_address = make_node_address(2803);
-    let tikv_config = make_tikv_runner_conf(2385, 2386, 20163);
+    #[fixture]
+    #[once]
+    pub fn mudb_fixture() -> Box<dyn DbManager> {
+        let data_dir = get_temp_dir();
+        let localhost = IpAddr::V4(Ipv4Addr::LOCALHOST);
 
-    let db_manager = DbManagerImpl::new_with_embedded_cluster(node_address, vec![], tikv_config)
-        .await
-        .unwrap();
+        let node_address = NodeAddress {
+            address: localhost,
+            port: 12803,
+        };
 
-    let (runtime, mut notifications) = start(Box::new(provider), config).await.unwrap();
+        let tikv_config = TikvRunnerConfig {
+            pd: PdConfig {
+                peer_url: IpAndPort {
+                    address: localhost,
+                    port: 12385,
+                },
+                client_url: IpAndPort {
+                    address: localhost,
+                    port: 12386,
+                },
+                data_dir: data_dir.join("/pd_data_dir").display().to_string(),
+                log_file: Some(data_dir.join("/pd_log").display().to_string()),
+            },
+            node: TikvConfig {
+                cluster_url: IpAndPort {
+                    address: localhost,
+                    port: 20163,
+                },
+                data_dir: data_dir.join("/tikv_data_dir").display().to_string(),
+                log_file: Some(data_dir.join("/tikv_log").display().to_string()),
+            },
+        };
 
-    runtime
-        .add_functions(functions.clone().into_values().into_iter().collect())
-        .await
-        .unwrap();
+        Box::new(
+            block_on!(DbManagerImpl::new_with_embedded_cluster(
+                node_address,
+                vec![],
+                tikv_config,
+            ))
+            .unwrap(),
+        )
+    }
 
-    let usages = Arc::new(Mutex::new(HashMap::new()));
+    #[fixture]
+    pub fn runtime_fixture<'a>(
+        _install_wasm32_wasi_target_fixture: (),
+        _build_test_funcs_fixture: (),
+        mudb_fixture: &'a Box<dyn DbManager>,
+    ) -> (
+        Box<dyn Runtime>,
+        &'a Box<dyn DbManager>,
+        Box<dyn AssemblyProvider>,
+        Arc<tokio::sync::Mutex<HashMap<StackID, Usage>>>,
+    ) {
+        let assembly_provider = MapAssemblyProvider::default();
 
-    tokio::spawn({
-        let usages = usages.clone();
-        async move {
-            loop {
-                match notifications.recv().await {
-                    None => continue,
-                    Some(n) => match n {
-                        Notification::ReportUsage(stack_id, usage) => {
-                            let mut map = usages.lock().await;
-                            if let Entry::Vacant(e) = map.entry(stack_id) {
-                                e.insert(usage);
-                            } else {
-                                *map.get_mut(&stack_id).unwrap() += usage;
+        let config = RuntimeConfig {
+            cache_path: get_temp_dir(),
+            include_function_logs: true,
+        };
+
+        let (runtime, mut notifications) =
+            block_on!(start(Box::new(assembly_provider.clone()), config)).unwrap();
+
+        let usages = Arc::new(tokio::sync::Mutex::new(HashMap::new()));
+
+        tokio::spawn({
+            let usages = usages.clone();
+            async move {
+                loop {
+                    if let Some(n) = notifications.recv().await {
+                        match n {
+                            Notification::ReportUsage(stack_id, usage) => {
+                                let mut map = usages.lock().await;
+                                if let Entry::Vacant(e) = map.entry(stack_id) {
+                                    e.insert(usage);
+                                } else {
+                                    *map.get_mut(&stack_id).unwrap() += usage;
+                                }
                             }
                         }
-                    },
+                    }
                 }
             }
-        }
-    });
+        });
 
-    (runtime, Box::new(db_manager), usages)
+        (runtime, mudb_fixture, Box::new(assembly_provider), usages)
+    }
+}
+
+//pub async fn create_runtime<'a>(
+//    projects: &'a [Project<'a>],
+//) -> (
+//    Box<dyn Runtime>,
+//    Box<dyn DbManager>,
+//    Arc<Mutex<HashMap<StackID, Usage>>>,
+//) {
+//    let config = RuntimeConfig {
+//        cache_path: get_temp_dir(),
+//        include_function_logs: true,
+//    };
+//
+//    let (functions, provider) = create_map_function_provider(projects).await.unwrap();
+//    let (runtime, mut notifications) = start(Box::new(provider), config).await.unwrap();
+//
+//    runtime
+//        .add_functions(functions.clone().into_values().into_iter().collect())
+//        .await
+//        .unwrap();
+//
+//    let usages = Arc::new(Mutex::new(HashMap::new()));
+//
+//    tokio::spawn({
+//        let usages = usages.clone();
+//        async move {
+//            loop {
+//                match notifications.recv().await {
+//                    None => continue,
+//                    Some(n) => match n {
+//                        Notification::ReportUsage(stack_id, usage) => {
+//                            let mut map = usages.lock().await;
+//                            if let Entry::Vacant(e) = map.entry(stack_id) {
+//                                e.insert(usage);
+//                            } else {
+//                                *map.get_mut(&stack_id).unwrap() += usage;
+//                            }
+//                        }
+//                    },
+//                }
+//            }
+//        }
+//    });
+//
+//    (runtime, Box::new(db_manager), usages)
+//}
+
+pub fn create_project<'a>(
+    name: &'a str,
+    functions: &'a [&'a str],
+    memory_limit: &Option<byte_unit::Byte>,
+) -> Project<'a> {
+    let memory_limit = memory_limit
+        .unwrap_or_else(|| byte_unit::Byte::from_unit(100.0, byte_unit::ByteUnit::MB).unwrap());
+
+    Project {
+        name,
+        path: Path::new(&format!("tests/funcs/{name}")).into(),
+        id: AssemblyID {
+            stack_id: StackID::SolanaPublicKey(rand::random()),
+            assembly_name: name.into(),
+        },
+        memory_limit,
+        functions,
+    }
+}
+
+pub async fn create_and_add_projects<'a>(
+    definitions: &'a [(&'a str, &'a [&'a str], Option<byte_unit::Byte>)],
+    runtime: &Box<dyn Runtime>,
+) -> Result<Vec<Project<'a>>> {
+    let mut projects = vec![];
+
+    for (name, funcs, mem_limit) in definitions.into_iter() {
+        projects.push(create_project(name, funcs, mem_limit));
+    }
+
+    let functions = read_wasm_functions(&projects).await?;
+    let function_defs = functions.clone().into_values().into_iter().collect();
+    runtime.add_functions(function_defs).await?;
+
+    Ok(projects)
+}
+
+pub fn make_request<'a>(
+    body: Cow<'a, [u8]>,
+    headers: Vec<Header<'a>>,
+    path_params: HashMap<Cow<'a, str>, Cow<'a, str>>,
+    query_params: HashMap<Cow<'a, str>, Cow<'a, str>>,
+) -> musdk_common::Request<'a> {
+    musdk_common::Request {
+        method: musdk_common::HttpMethod::Get,
+        headers,
+        body,
+        path_params,
+        query_params,
+    }
 }
