480dc0caa10879522a42f733e712ae7e11422aa2
Mohammad H. Bahrampour <bahrampour@protonmail.com>
2023-02-01 17:16:11 +0330

    refactor(mui): delete everything related to mui

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,17 +1,14 @@
 import type { AppProps } from "next/app";
 
 import Header from "@/features/layout/Header";
-import ThemeWrapper from "@/features/themes/ThemeWrapper/ThemeWrapper";
 import WalletWrapper from "@/features/wallet/WalletWrapper/WalletWrapper";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeWrapper>
-      <WalletWrapper>
-        <Header />
-        <Component {...pageProps} />
-      </WalletWrapper>
-    </ThemeWrapper>
+    <WalletWrapper>
+      <Header />
+      <Component {...pageProps} />
+    </WalletWrapper>
   );
 }
