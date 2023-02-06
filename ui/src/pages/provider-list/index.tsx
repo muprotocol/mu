eab7d88ad25efe70fd38a42ddf9fde043f667212
naixatwork <naixatwork@gmail.com>
2023-02-06 20:10:48 +0330

    refactor(useAccount): changed the useAccount to accept promise functions from marketplace

--- ui/src/pages/provider-list/index.tsx
+++ ui/src/pages/provider-list/index.tsx
@@ -1,20 +1,9 @@
-import useAllAccounts, {useAccount} from "@/features/marketplace/useAllAccounts/useAllAccounts";
-import useWithLoading from "@/hooks/useWithLoading/useWithLoading";
 import useMarketplace from "@/features/marketplace/useMarketplace/useMarketplace";
-import {MarketplaceAccount} from "@/features/marketplace/marketplace.type";
-import {useState} from "react";
+import useAccount from "@/features/marketplace/useAccount/useAccount";
 
 export default function ProviderList() {
-    const [providers, isLoading] = useAllAccounts<"provider">(
-        "provider",
-        console.log,
-    );
-
     const {marketplace} = useMarketplace();
-    const [providers1, isLoading1] = useAccount<MarketplaceAccount<"provider">[]>(marketplace.account.provider.all(), []);
-
-    const [providers2, setProviders2] = useState<MarketplaceAccount<"provider">[]>([]);
-    const [isLoading2] = useWithLoading(marketplace.account.provider.all(), setProviders2);
+    const [providers, isLoading] = useAccount(marketplace.account.provider.all(), []);
 
     return (
         <div className="container mx-auto">
@@ -23,7 +12,7 @@ export default function ProviderList() {
             </div>
 
             <div className="flex flex-col gap-2">
-                {providers2.map((provider) => {
+                {providers.map((provider) => {
                     return (
                         <div
                             className="rounded bg-black p-5 text-white"
