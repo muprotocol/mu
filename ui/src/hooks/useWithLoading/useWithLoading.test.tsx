e140eadce742b0204bed65eac5fe80aeaa2c330d
naixatwork <naixatwork@gmail.com>
2023-02-07 21:04:19 +0330

    test(useWithLoading): installed @testing-library/react-hooks to test useWithLoading hook for its resolve

--- ui/src/hooks/useWithLoading/useWithLoading.test.tsx
+++ ui/src/hooks/useWithLoading/useWithLoading.test.tsx
@@ -1,18 +1,22 @@
-import {render, renderHook, screen, waitFor} from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import {describe, expect, test} from "vitest";
 
 import useWithLoading from "@/hooks/useWithLoading/useWithLoading";
+import {renderHook} from "@testing-library/react-hooks";
 
 describe("useWithLoading", () => {
-  test("it should be true by default", () => {
-    const promise = new Promise(() => "test");
-    const { result } = renderHook(() => useWithLoading(promise));
-    const [isLoading] = result.current;
+    test("it should be true by default", () => {
+        const promise = new Promise(() => "test");
+        const {result} = renderHook(() => useWithLoading(promise));
+        const [isLoading] = result.current;
 
-    expect(isLoading).toBeTruthy();
-  });
+        expect(isLoading).toBeTruthy();
+    });
 
-  test("it should be false on promise resolves", () => {
-    expect("pending").toBe(false);
-  });
+    test("it should be false on promise resolves", async () => {
+        const promise = Promise.resolve("test");
+        const {result, waitForNextUpdate} = renderHook(() => useWithLoading(promise));
+        await waitForNextUpdate();
+        const [isLoading] = result.current;
+        expect(isLoading).toBeFalsy();
+    });
 });
