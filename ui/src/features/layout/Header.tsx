1102787adabaeb0f6cd1ae2c646db3c7ab4c325d
naixatwork <naixatwork@gmail.com>
2023-02-07 19:38:50 +0330

    feat(header): have header background color and shadow and made menu icon bigger

--- ui/src/features/layout/Header.tsx
+++ ui/src/features/layout/Header.tsx
@@ -1,25 +1,28 @@
 import Image from "next/image";
 
 import {Button} from "primereact/button"
-import { MuLogo } from "@/components/muLogo/muLogo";
+import {MuLogo} from "@/components/muLogo/muLogo";
 
 import Drawer from "@/features/layout/Drawer/Drawer/Drawer";
 import useDrawer from "@/features/layout/Drawer/useDrawer";
 import WalletButton from "@/features/wallet/WalletButton/WalletButton";
 
 export default function Header() {
-  const { isOpen, openDrawer, closeDrawer } = useDrawer();
+    const {isOpen, openDrawer, closeDrawer} = useDrawer();
 
-  return (
-    <header className="flex! container mx-auto flex items-center gap-5 py-5">
-      <Drawer isOpen={isOpen} closeDrawer={closeDrawer} />
-      <Button icon="pi pi-bars" className="p-button-rounded p-button-text lg:!hidden" onClick={openDrawer} />
-      <div>
-        <MuLogo />
-      </div>
-      <div className="ml-auto">
-        <WalletButton />
-      </div>
-    </header>
-  );
+    return (
+        <header className="w-full bg-slate-100 mb-10 shadow">
+            <div className="flex! container mx-auto flex items-center gap-5 py-5 ">
+                <Drawer isOpen={isOpen} closeDrawer={closeDrawer}/>
+                <Button icon="pi pi-bars !text-xl" className="p-button-rounded p-button-text lg:!hidden"
+                        onClick={openDrawer}/>
+                <div>
+                    <MuLogo/>
+                </div>
+                <div className="ml-auto">
+                    <WalletButton/>
+                </div>
+            </div>
+        </header>
+    );
 }
