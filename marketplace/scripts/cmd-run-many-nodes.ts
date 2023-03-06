8f61052c39c5cc1d130088f88a20babf08e45198
Arshia Ghafoori <arshia001@live.com>
2023-03-06 15:18:09 +0330

    Replace gossip with tikv-backed membership list

--- marketplace/scripts/cmd-run-many-nodes.ts
+++ marketplace/scripts/cmd-run-many-nodes.ts
@@ -4,10 +4,9 @@ import { ProcessMultiplexer } from "./process-multiplexer";
 import { asyncMain, run, sleep } from "./util";
 
 asyncMain(async () => {
-    const numSeeds = parseInt(argv[2]) ?? 3;
-    const numTotal = parseInt(argv[3]) ?? 10;
+    const numTotal = parseInt(argv[2]) ?? 10;
 
-    console.log(`Starting ${numSeeds} seeds and ${numTotal - numSeeds} normal nodes`);
+    console.log(`Starting ${numTotal} nodes`);
 
     const executorPath = path.resolve(__dirname, "../../executor");
     const executorManifestPath = path.resolve(executorPath, "Cargo.toml");
@@ -18,28 +17,21 @@ asyncMain(async () => {
 
     let muxer = new ProcessMultiplexer();
 
-    var seedEnvVars = "";
-
     for (let i = 0; i < numTotal; ++i) {
-        seedEnvVars += `MU__INITIAL_CLUSTER[${i}]__ADDRESS=127.0.0.1 ` +
-            `MU__INITIAL_CLUSTER[${i}]__GOSSIP_PORT=${20100 + i} ` +
-            `MU__INITIAL_CLUSTER[${i}]__PD_PORT=${20200 + i} `;
-
         let tempDir = `/tmp/mu-executor/${i}/`;
         run(`mkdir -p '${tempDir}' && cp '${configFilePath}' '${tempDir}' && cp '${devConfigFilePath}' '${tempDir}'`);
 
-        let port = 20100 + i;
-        let name = i < numSeeds ? `seed-${i + 1}` : `node-${i + 1}`;
+        let port = 20100 + i + 1;
+        let name = `node-${i + 1}`;
 
         muxer.spawnNew(
             `env -C ${tempDir} ` +
             `MU__CONNECTION_MANAGER__LISTEN_PORT=${port} ` +
             `MU__GATEWAY_MANAGER__LISTEN_PORT=${port} ` +
-            seedEnvVars +
             ` cargo run --manifest-path ${executorManifestPath}`,
             name);
 
-	await sleep(5);
+        await sleep(5);
     }
 
     await muxer.waitForAllWithSigint();
